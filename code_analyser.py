b1e70982e5bf7c5eca3aa5e22dbb78b6410c27b3
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2017-01-03 15:30:12 +0530

    added mypy for testing types and checking for type consistency

--- code_analyser.py
+++ code_analyser.py
@@ -9,8 +9,15 @@ class AnalyseCode(object):
         attr2 (:obj:`int`, optional): Description of `attr2`.
 
         this is for ref and will be deleted later
-        take the conventions from the google code conventions"""
-    def __init__(self, code: str, _tree: str = ""):
+        take the conventions from the google code conventions
+
+    TODO:
+        fix mypy errors:
+            code_analyser.py: note: In member "__init__" of class "AnalyseCode":
+code_analyser.py:17: error: Incompatible types in assignment (expression has type "str", variable has type "AST")
+code_analyser.py: note: In member "tree" of class "AnalyseCode":
+code_analyser.py:21: error: Incompatible return value type (got "AST", expected "str")"""
+    def __init__(self, code: str, _tree: str = "") -> None:
         super(AnalyseCode, self).__init__()
         self.code = code
         self._tree = ast.parse(code)
@@ -21,5 +28,17 @@ class AnalyseCode(object):
         return self._tree
 
     def has_list_append(self) -> bool:
-        if self._tree.find("Call"):
-            return "append" in self._tree
+        t = str(self._tree)
+        if t.find("Call"):
+            return "append" in t
+
+    def has_proper_classes(self):
+        pass
+
+
+if __name__ == '__main__':
+    code='''
+my_list=['raindrops n roses', 'whiskers on kittens']
+my_list.append('bright copper kettles')'''
+    ac = AnalyseCode(code)
+    print(ac.has_list_append())
